98f3614de6711b44cbe76f7c7fde1d2ca3e8ae63
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-12-18 16:41:19 -0300

    creating mini message board before deploying

---
+++ routes/index.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message" });
+});
+
+/* POST new message form data. */
+router.post("/new", function (req, res, next) {
+  console.log(req.body);
+  // Access form data from req.body
+  const author = req.body.author;
+  const messageText = req.body.message;
+
+  messages.push({
+    text: messageText,
+    user: author,
+    added: new Date(),
+
+
+
+  });
+
+  res.redirect("/");
+
+
+})
+
+
+
+module.exports = router;
